0ccea0722a40b7b1aa8f8c4e9f8719a6be04d6c8
shoc71 <supersonicwind6@gmail.com>
2025-02-12 13:10:19 -0500

    backup push

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -14,21 +14,21 @@ let searchHistory: string[] = [];
 router.post('/', async (req, res) => {
   // TODO: GET weather data from city name
   try {
-    const { city } = req.body;
-    if (!city) {
+    const cityName  = req.body.cityName;
+    if (!cityName) {
       return res.status(400).json({ error: 'City name is required' });
     }
 
-    const weather = await new WeatherService(process.env.WEATHER_API_KEY || '', city).getWeatherForCity();
+    const weather = await new WeatherService(process.env.WEATHER_API_KEY || '', cityName).getWeatherForCity(cityName);
     if (!weather) {
       return res.status(404).json({ error: 'Weather data not found' });
     }
 
     // TODO: save city to search history
     // Save city to search history
-    if (!searchHistory.includes(city)) {
-      searchHistory.push(city)
-    }
+    // if (!searchHistory.includes(city)) {
+    //   searchHistory.push(city)
+    // }
 
     res.json(weather);
   } catch (error) {
